2dce37fc357971af3f5e44d830c8ddca23074fd5
andres <afcanop@gmail.com>
2016-04-12 20:38:26 -0500

    cambio en el menu de productos

--- application/view/contenido/ContenidoAdmTiendaCatalogo.php
+++ application/view/contenido/ContenidoAdmTiendaCatalogo.php
@@ -7,94 +7,105 @@
                         <div class="panel-heading" role="tab" id="headingOne">
                             <h4 class="panel-title">
                                 <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                    producto
-                                </a>
-                            </h4>
+                                  Registrar  productos
+                              </a>
+                          </h4>
+                      </div>
+                      <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                        <div class="panel-body">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group form-animate-text">
+                                        <input type="text" class="form-text"   name="PrimerNombre" required>
+                                        <span class="bar"></span>
+                                        <label><span class="glyphicon glyphicon-pencil"></span> Registrar primer nombre del nuevo administrador </label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group form-animate-text">
+                                        <input type="text" class="datepicker form-text dateAnimate" name="date" required>
+                                        <span class="bar"></span>
+                                        <label><span class="fa fa-calendar"></span> Fecha de registristo</label>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="exampleInputFile"><samp class="glyphicon glyphicon-picture"></samp> IMAGEN DEL PROYECTO</label>
+                                        <input type="file" id="exampleInputFile">
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
-                            <div class="panel-body">
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="form-group form-animate-text">
-                                            <input type="text" class="form-text"   name="PrimerNombre" required>
-                                            <span class="bar"></span>
-                                            <label><span class="glyphicon glyphicon-pencil"></span> Registrar primer nombre del nuevo administrador </label>
-                                        </div>
-                                    </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <label class="label label-default">Descripción producto </label>
+                                <br>
+                                <textarea class="form-control" rows="5" id="comment"></textarea>
+                            </div>
+                        </div>
+                        <BR>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                      <label for="sel1">Categoria del producto</label>
+                                      <select class="form-control" id="sel1">
+                                        <OPTION></OPTION>
+                                        <option>GORRAR</option>
+                                        <option>VESTIMENTA</option>
+                                        <option>AEROSOLES</option>
+                                        <option>CALZADO</option>
+                                    </select>
                                 </div>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <div class="form-group form-animate-text">
-                                            <input type="text" class="datepicker form-text dateAnimate" name="date" required>
-                                            <span class="bar"></span>
-                                            <label><span class="fa fa-calendar"></span> Fecha de registristo</label>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="form-group form-animate-text">
-                                            <input type="text" class="datepicker form-text dateAnimate" name="date" required>
-                                            <span class="bar"></span>
-                                            <label><span class="fa fa-calendar"></span> Fecha de vencimiento</label>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <label for="exampleInputFile"><samp class="glyphicon glyphicon-picture"></samp> File input</label>
-                                            <input type="file" id="exampleInputFile">
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <label for="exampleInputFile"><samp class="glyphicon glyphicon-picture"></samp> Categoria</label>
-                                        <select class="js-example-basic-single">
-                                            <option value="AL">Alabama</option>
-                                            <option value="WY">Wyoming</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <label class="label label-default">Descripción producto </label>
-                                        <br>
-                                        <textarea class="form-control" rows="5" id="comment"></textarea>
-                                    </div>
-                                    <div class="col-md-3">
-                                        <div class="form-group form-animate-text">
-                                            <input type="text" class="form-text"   name="PrimerNombre" required>
-                                            <span class="bar"></span>
-                                            <label><span class="glyphicon glyphicon-pencil"></span>color </label>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-3">
-                                        <label><span class="fa fa-users"></span> roles</label>
-                                        <select class="js-example-basic-multiple" multiple="multiple">
-                                            <optgroup label="ASOCIAR HA:">
-                                                <option value="AK">Administrador</option>
-                                                <option value="HI">Guia</option>
-                                                <option value="HI">Traductor</option>
-                                            </optgroup>
+                            </div>
+                        </div>
 
-                                        </select>
-                                    </div>
-                                </div>
+                        <!--calzado -->
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                 <label for="sel1">Categoria del calzado</label>
+                                 <select class="form-control" id="sel1">
+                                    <optgroup>INFANTILES Y NIÑOS</optgroup>
+                                    <option>20</option>
+                                    <option>21</option>
+                                    <option>22</option>
+                                    <option>23</option>
+                                    <option>24</option>
+                                    <option>25</option>
+                                    <option>26</option>
+                                    <option>27</option>
+                                    <option>27 1/2</option>
+                                    <option>28</option>
+                                    <option>28 1/2</option>
+                                    <option>29</option>
+                                    <option>30</option>
+                                    <optgroup>jovenes</optgroup>
+                                </select>
                             </div>
                         </div>
                     </div>
-                    <div class="panel panel-default">
-                        <div class="panel-heading" role="tab" id="headingTwo">
-                            <h4 class="panel-title">
-                                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                    Collapsible Group Item #2
-                                </a>
-                            </h4>
-                        </div>
-                        <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
-                            <div class="panel-body">
-                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                            </div>
-                        </div>
-                    </div>
+
+
+
                 </div>
             </div>
         </div>
+        <div class="panel panel-default">
+            <div class="panel-heading" role="tab" id="headingTwo">
+                <h4 class="panel-title">
+                    <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        Collapsible Group Item #2
+                    </a>
+                </h4>
+            </div>
+            <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+                <div class="panel-body">
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
