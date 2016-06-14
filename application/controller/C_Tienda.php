135d452156540bdf1586e8f7a0976f10da395bae
afcanop <alejo.jko@gmail.com>
2016-06-14 16:53:00 -0500

    listar productos de a tres

--- application/controller/C_Tienda.php
+++ application/controller/C_Tienda.php
@@ -21,61 +21,31 @@ class C_Tienda extends Controller
     public function Listar()
     {
      $datos = ["data"=>[]];
+     $fila = "";
+     $contFilas = 0;
      foreach ($this->MldProductos->ListarProductosPublicos() as $value) {
-            $datos ["data"][]=[
-             
-"<div class='row'>
-  <div class='col-sm-6 col-md-4'>
-    <div class='thumbnail text-center'>
-      <div class='caption '>
-        <h2>". $value->IDPRODUCTOS." ".$value->NOMBREPRODUCTO."</h2>
-        '<img src=".$value->IMAGEN." style=' height: 100px; width: 100px;'> 
-        <br>
-        <stronge>Caracteristicas</stronge>
-        <p>Color: ".$value->Color."</p>
-        <P>Marca: ".$value->Marca."</p>
-        <p>Precio:".$value->Precio."</p>
-        <p>Categoria:".$value->NombreCategoria."</p>
-        <stronge>Descripción: </stronge>
-        <p>".$value->DESCRIPCION."</p>
-      </div>
-    </div>
-  </div>
-  <div class='col-sm-6 col-md-4'>
-    <div class='thumbnail text-center'>
-      <div class='caption '>
-        <h2>". $value->IDPRODUCTOS." ".$value->NOMBREPRODUCTO."</h2>
-        '<img src=".$value->IMAGEN." style=' height: 100px; width: 100px;'> 
-        <br>
-        <stronge>Caracteristicas</stronge>
-        <p>Color: ".$value->Color."</p>
-        <P>Marca: ".$value->Marca."</p>
-        <p>Precio:".$value->Precio."</p>
-        <p>Categoria:".$value->NombreCategoria."</p>
-        <stronge>Descripción: </stronge>
-        <p>".$value->DESCRIPCION."</p>
-      </div>
-    </div>
-  </div>
-  <div class='col-sm-6 col-md-4'>
-    <div class='thumbnail text-center'>
-      <div class='caption '>
-        <h2>". $value->IDPRODUCTOS." ".$value->NOMBREPRODUCTO."</h2>
-        '<img src=".$value->IMAGEN." style=' height: 100px; width: 100px;'> 
-        <br>
-        <stronge>Caracteristicas</stronge>
-        <p>Color: ".$value->Color."</p>
-        <P>Marca: ".$value->Marca."</p>
-        <p>Precio:".$value->Precio."</p>
-        <p>Categoria:".$value->NombreCategoria."</p>
-        <stronge>Descripción: </stronge>
-        <p>".$value->DESCRIPCION."</p>
-      </div>
-    </div>
-  </div>
-</div>",
-
-            ];
+      // $datos ["data"][]=[]
+        if($contFilas % 3 == 0){
+          $datos['data'][] = ["<div class='row'>$fila</div>"];
+          $fila = "";
+        }
+            $fila .= "<div class='col-sm-6 col-md-4'>" . 
+                                  "<div class='thumbnail text-center'>" .
+                                    "<div class='caption '>" .
+                                      "<h2>". $value->IDPRODUCTOS." ".$value->NOMBREPRODUCTO."</h2>" .
+                                      "'<img src=".$value->IMAGEN." style=' height: 100px; width: 100px;'>" .
+                                      "<br>" .
+                                      "<stronge>Caracteristicas</stronge>".
+                                      "<p>Color: ".$value->Color."</p>" . 
+                                      "<P>Marca: ".$value->Marca."</p>" . 
+                                      "<p>Precio:".$value->Precio."</p>" . 
+                                      "<p>Categoria:".$value->NombreCategoria."</p>" . 
+                                      "<stronge>Descripción: </stronge>" . 
+                                      "<p>".$value->DESCRIPCION."</p>" . 
+                                    "</div>" .
+                                  "</div>" .
+                                "</div>";
+        $contFilas ++;
      }
     echo json_encode($datos);
      
