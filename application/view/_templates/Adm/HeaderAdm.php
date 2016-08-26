cc8df4defde303dc886a1813412785d9820a0e53
andres <afcanop@gmail.com>
2016-08-25 23:04:04 -0500

    -se corrigio error con el modal de mi perfil el cual se truncada con el modal de modificar

--- application/view/_templates/Adm/HeaderAdm.php
+++ application/view/_templates/Adm/HeaderAdm.php
@@ -57,7 +57,7 @@
 
                                 <ul class="dropdown-menu user-dropdown">
                                     <li>
-                                    <a data-toggle="modal" data-target="#myModal" onclick="usuarios.PerFil(<?= $_SESSION["codigo"] ?>)" ><span class="glyphicon glyphicon-user"></span> Mi Perfil</a></li>
+                                    <a data-toggle="modal" data-target="#MiPerfil" onclick="usuarios.PerFil(<?= $_SESSION["codigo"] ?>)" ><span class="glyphicon glyphicon-user"></span> Mi Perfil</a></li>
                                     <li><a href="<?php URL ?> adm/CerrarSession"><span class="glyphicon glyphicon-off"> </span> Salir</a></li>
                                 </ul>
                                 
@@ -68,7 +68,7 @@
             </div>
         </nav>
         <!-- Modal -->
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+<div class="modal fade" id="MiPerfil" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
