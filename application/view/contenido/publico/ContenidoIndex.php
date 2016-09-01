d2dc80eae9e97ddf9f533bd4497e5cb09458c4e5
afcanop <afcanop@gmail.com>
2016-09-01 10:28:52 -0500

    se organizo la ortografia de la página publica

--- application/view/contenido/publico/ContenidoIndex.php
+++ application/view/contenido/publico/ContenidoIndex.php
@@ -13,14 +13,14 @@
                     <nav class="mbr-navbar__menu-box mbr-navbar__menu-box--inline-right">
                         <div class="mbr-navbar__column">
                             <ul class="mbr-navbar__items mbr-navbar__items--right mbr-buttons mbr-buttons--freeze mbr-buttons--right btn-decorator mbr-buttons--active">
-                                <li class="mbr-navbar__item"><a class="mbr-buttons__link btn text-white" href="#QueEs">¿QUÉ ES EL GRAFFITOUR?</a></li>
-                                <li class="mbr-navbar__item"><a class="mbr-buttons__link btn text-white" href="#Solicitud">PAQUETES</a></li>
-                                <li class="mbr-navbar__item"><a class="mbr-buttons__link btn text-white" href="#Informacion">INFORMACIÓN</a></li> 
-                                <li class="mbr-navbar__item"><a class="mbr-buttons__link btn text-white" href="#Productos">PRODUCTOS</a></li>
-                                <li class="mbr-navbar__item"><a class="mbr-buttons__link btn text-white" href="#Noticias">NOTICIAS</a></li>
-                                 <li class="mbr-navbar__item"><a class="mbr-buttons__link btn text-white" href="#Peticion">PETICÍON</a></li>
-                                <li class="mbr-navbar__item"><a class="mbr-buttons__link btn text-white" href="#Social">REDES SOCIALES</a></li>
-                                <li class="mbr-navbar__item"><a class="mbr-buttons__link btn text-white" href="#UBICANOS">UBICANOS</a></li>
+                                <li class="mbr-navbar__item"><a class="mbr-buttons__link btn text-white" href="#QueEs">¿Que es el GRAFFITOUR?</a></li>
+                                <li class="mbr-navbar__item"><a class="mbr-buttons__link btn text-white" href="#Solicitud">Paquetes</a></li>
+                                <li class="mbr-navbar__item"><a class="mbr-buttons__link btn text-white" href="#Informacion">Información</a></li> 
+                                <li class="mbr-navbar__item"><a class="mbr-buttons__link btn text-white" href="#Productos">Productos</a></li>
+                                <li class="mbr-navbar__item"><a class="mbr-buttons__link btn text-white" href="#Noticias">Noticias</a></li>
+                                 <li class="mbr-navbar__item"><a class="mbr-buttons__link btn text-white" href="#Peticion">Solicitud</a></li>
+                                <li class="mbr-navbar__item"><a class="mbr-buttons__link btn text-white" href="#Social">Redes sociales</a></li>
+                                <li class="mbr-navbar__item"><a class="mbr-buttons__link btn text-white" href="#UBICANOS">Ubicanos</a></li>
                                 <li class="mbr-navbar__item"><a class="mbr-buttons__link btn text-white" href="<?php echo URL ?>Adm">INGRESAR</a></li>
                                 <li class="mbr-navbar__item">
                                  <div  id="google_translate_element"></div><script type="text/javascript">
@@ -47,7 +47,7 @@
             <div class="mbr-box mbr-box--stretched"><div class="mbr-box__magnet mbr-box__magnet--center-left">
                 <div class="row"><div class=" col-sm-6">
                     <div class="mbr-hero animated fadeInUp">
-                        <h1 class="mbr-hero__text">¿QUÉ ES EL GRAFFITOUR?</h1>
+                        <h1 class="mbr-hero__text">¿Que es el GRAFFITOUR?</h1>
                         <p class="mbr-hero__subtext">El Graffitour es un recorrido histórico, estético y político que diseñaron y ejecutan en la Comuna 13 de Medellín, algunos artistas callejeros del movimiento Hip Hop de esa zona de la ciudad. A través de graffitis, se dan a conocer las historias que mueven e inspiran la esperanza y la búsqueda de mejores condiciones de vida para la comunidad, también se cuentas las historias de iniquidades y violencias históricas que aún son presente en la zona. Esta es una iniciativa liderada por el colectivo Casa Kolacho.
                             <br></p>
                         </div>
@@ -60,13 +60,13 @@
     <section id="Solicitud" class="pricing-table-1 col-4" id="pricing-table1-86" style="background-color: rgb(240, 240, 240);">
         <div class="container">
             <div class="row">
-                <h1 class="text-center">PAQUETES</h1>
+                <h1 class="text-center">Paquetes</h1>
             </div>
             <br>
             <div class="row">
                 <div class="text-center">
                     <div class="plan orange">
-                        <h3>PAQUETES 1</h3>
+                        <h3>Paquete 1</h3>
                         <strong><p>$ 25.000</p> </strong>
                         <small>por persona</small>
                         <div><ul><li><strong>1 a 10</strong> Personas</li></div>
@@ -74,7 +74,7 @@
                 </div>
                 <div class="text-center">
                     <div class="plan favorite">
-                        <h3>PAQUETES 3</h3>
+                        <h3>Paquete 3</h3>
                         <strong><p>$ 300.000</p> </strong>
                         <small>por el grupo</small>
                         <div><ul><li><strong>16 a 20</strong> Personas</li><li></div>
@@ -82,7 +82,7 @@
                 </div>
                 <div class="text-center">
                     <div class="plan blue">
-                        <h3>PAQUETES 2</h3>
+                        <h3>Paquete 2</h3>
                         <strong><p>$ 250.000</p></strong>
                         <small>por el grupo</small>
                         <div><ul><li><strong>11 a 15</strong> Personas</li><li></div>
@@ -94,7 +94,7 @@
     <!--informacion-->
     <div id="Informacion">
         <section  class="mbr-section mbr-section--relative mbr-parallax-background" id="msg-box5-79" style="background-image: url(asistente/img/2.jpg);">
-            <div class="mbr-overlay" style="opacity: 0.5; background-color: rgb(0, 168, 133);"></div>
+            <div class="mbr-overlay" style="opacity: 0.82; background-color: rgb(7, 37, 32);"></div>
             <div class="mbr-section__container mbr-section__container--isolated container">
                 <div class="row">
                     <div class="mbr-box mbr-box--fixed mbr-box--adapted">
@@ -104,13 +104,13 @@
                         <div class="mbr-box__magnet mbr-class-mbr-box__magnet--center-left col-sm-6 mbr-section__right">
                             <div class="mbr-section__container mbr-section__container--middle">
                                 <div class="mbr-header mbr-header--auto-align mbr-header--wysiwyg">
-                                    <h3 class="mbr-header__text">INFORMACIÓN</h3>
+                                    <h3 class="mbr-header__text" style="color:white">Información</h3>
 
                                 </div>
                             </div>
                             <div class="mbr-section__container mbr-section__container--middle">
                                 <div class="mbr-article mbr-article--auto-align mbr-article--wysiwyg">
-                                    <p>
+                                    <p style="color:white">
                                         El recorrido dura entre 3 y 4 horas, inicia en la Estación San Javier del metro y termina en las Escaleras Eléctricas de la Comuna 13 en el barrio Independencias numero I, cabe anotar que el recorrido en su totalidad es caminando. <br>
                                         Consejos prácticos para el Graffitour:
                                         Llevar una cantimplora con hidratación o dinero para poder comprar liquido en las tiendas.
@@ -138,7 +138,7 @@
     <div id="Productos">
         <section class="content-2 col-12" id="features1-75" style="background-color: rgb(255, 255, 255);">
             <div class="container">
-                <h2 class="text-center"> PRODUCTOS</h2>
+                <h2 class="text-center"> Productos</h2>
           <div id="ListaProductos">   
           </div>
  </section>    
@@ -150,7 +150,7 @@
 
             <div class="container">
                 <div class="row">
-                    <h2 class="text-center">NOTICIAS</h2>
+                    <h2 class="text-center">Noticias</h2>
                 </div>
         <!-- demo -->
         <!-- demo -->
@@ -382,7 +382,7 @@
                     <div class="row">
                         <div class="col-sm-8 col-sm-offset-2" data-form-type="formoid">
                             <div class="mbr-header mbr-header--center mbr-header--std-padding">
-                                <h2 class="mbr-header__text">SOLICITUD DE UN TOUR</h2>
+                                <h2 class="mbr-header__text">Solicitud de un tour</h2>
                             </div>
                             <form  data-form-title="CONTACT FORM" id="FrmSolicitud">
                                 <input type="hidden" value="" data-form-email="true">
@@ -393,7 +393,7 @@
                                             <input type="text" class="form-control" id="txtPrimerNombre" name="txtPrimerNombre" required="ingrese el nombre" placeholder="Nombre responsable" data-form-field="Name">
                                         </div>
                                         <div class="col-md-6">
-                                            <label>Segundo Nombre <span style="color: red;">*</span></label>
+                                            <label>Segundo Nombre</label>
                                             <input type="text" class="form-control" id="txtSegundoNombre" name="txtSegundoNombre" required="" placeholder="Nombre responsable" data-form-field="Name">
                                         </div>
                                     </div>
@@ -409,11 +409,11 @@
                                     </div>
                                     <div class="row">
                                         <div class="col-md-6">
-                                            <label> Correo Electronico <span style="color: red;">*</span></label>
+                                            <label> Correo Electrónico <span style="color: red;">*</span></label>
                                             <input type="email" class="form-control" name="txtEmail"  id="txtEmail"    required="" placeholder=" Ej: Example@Example.com" data-form-field="Email">
                                         </div>
                                         <div class="col-md-6">
-                                            <label> Cantidad de personsa <span style="color: red;">*</span></label>
+                                            <label> Cantidad de personas <span style="color: red;">*</span></label>
                                             <input type="number" min="1" max="21" class="form-control" name="txtCantidadPersonas" id="txtCantidadPersonas" required="" placeholder="Ej: 4-21" data-form-field="Name">        
                                         </div>
                                     </div>
@@ -445,9 +445,9 @@
         <div class="mbr-section__container container">
             <div class="mbr-header mbr-header--inline row">
                 <div class="col-sm-4">
-                    <h3 class="mbr-header__text" style="color: white;">REDES SOCIALES</h3>
+                    <h3 class="mbr-header__text" style="color: white;">Redes sociales</h3>
                 </div>
-                <div class="mbr-social-icons mbr-social-icons--style-1 col-sm-8"><a class="mbr-social-icons__icon socicon-bg-twitter" title="Twitter" target="_blank" href="https://twitter.com/hashtag/graffitour"><i class="socicon socicon-twitter"></i></a> <a class="mbr-social-icons__icon socicon-bg-facebook" title="Facebook" target="_blank" href="https://www.facebook.com/grafitour/?fref=ts"><i class="socicon socicon-facebook"></i></a> <a class="mbr-social-icons__icon socicon-bg-google" title="Google+" target="_blank" href="https://plus.google.com/u/0/+Mobirise/posts"><i class="socicon socicon-google"></i></a> <a class="mbr-social-icons__icon socicon-bg-youtube" title="YouTube" target="_blank" href="http://www.youtube.com/channel/UCt_tncVAetpK5JeM8L-8jyw"><i class="socicon socicon-youtube"></i></a> <a class="mbr-social-icons__icon socicon-bg-instagram" title="Instagram" target="_blank" href="https://instagram.com/mobirise/"><i class="socicon socicon-instagram"></i></a></div>
+                <div class="mbr-social-icons mbr-social-icons--style-1 col-sm-8"><a class="mbr-social-icons__icon socicon-bg-twitter" title="Twitter" target="_blank" href="https://twitter.com/hashtag/graffitour"><i class="socicon socicon-twitter"></i></a> <a class="mbr-social-icons__icon socicon-bg-facebook" title="Facebook" target="_blank" href="https://www.facebook.com/grafitour/?fref=ts"><i class="socicon socicon-facebook"></i></a> <a class="mbr-social-icons__icon socicon-bg-youtube" title="YouTube" target="_blank" href="http://www.youtube.com/channel/UCt_tncVAetpK5JeM8L-8jyw"><i class="socicon socicon-youtube"></i></a></div>
             </div>
         </div>
     </section>
@@ -461,22 +461,26 @@
 
     <div class="mbr-section__container container">
         <div class="mbr-contacts mbr-contacts--wysiwyg row">
-            <div class="col-sm-6">
-            <h3 class="mbr-header__text" style="color: white;">UBICANOS</h3>
-             <iframe src="https://www.google.com/maps/embed?pb=!1m21!1m12!1m3!1d991.514663658735!2d-75.61533097300612!3d6.256003602630079!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m6!3e2!4m0!4m3!3m2!1d6.2558112999999995!2d-75.6142497!5e0!3m2!1ses-419!2s!4v1468458761758" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-         </div>
-         <div class="col-sm-6">
+            <h3 class="mbr-header__text" style="color: white;">Ubicanos</h3>
+            <div class="row">
+                <div class="col-md-6">
+                 <iframe src="https://www.google.com/maps/embed?pb=!1m21!1m12!1m3!1d991.514663658735!2d-75.61533097300612!3d6.256003602630079!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m6!3e2!4m0!4m3!3m2!1d6.2558112999999995!2d-75.6142497!5e0!3m2!1ses-419!2s!4v1468458761758" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                </div> 
+                <div class="col-md-6">
             <div class="row">
                 <div class="col-sm-5 col-sm-offset-1">
                     <p class="mbr-contacts__text"><strong>Dirección kasa kolacho</strong><br>
                         Carrera 97 #43-76, Medellín, Antioquia, Colombia<br>
                         <strong>Más Información</strong><br>
-                        Phone: 3128895564<br>
+                        Teléfono: 3128895564<br>
                         Whatsapp: 3128895564<br>
 
                     </div>
                 </div>
+            </div>   
             </div>
+            
+         
         </div>
     </div>
 </section>
