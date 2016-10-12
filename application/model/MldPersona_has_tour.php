8b0aa1e05ffb9399b1d4170a8789148340c9806d
afcanop <afcanop@gmail.com>
2016-10-12 00:10:03 -0500

    se agrego en el modelo depersona tour las metodos para consultar por año y por año con mes

--- application/model/MldPersona_has_tour.php
+++ application/model/MldPersona_has_tour.php
@@ -8,6 +8,8 @@
     private $HoraRegistro;
     private $FechaInicioReporte;
     private $FechaFinalReporte;
+    private $Anio;
+    private $Mes;
 
     //metodos magicos get y set
     public function __GET($atributo) {
@@ -37,12 +39,30 @@
        return $sth->execute();
    }
 
-   public function ConsultarFecha()
+   public function ConsultarFechaAnioMes()
    {
     $sql= 'CALL RU_InformeEntreFechas(?,?)';
     $sth = $this->db->prepare($sql);
     $sth->bindParam(1, $this->FechaInicioReporte);
     $sth->bindParam(2, $this->FechaFinalReporte);
+    $sth->execute();
+    return $sth->fetchAll();
+    }
+
+       public function ConsultarFechaAnio()
+   {
+    $sql= 'CALL RU_ReportePorAnio(?)';
+    $sth = $this->db->prepare($sql);
+    $sth->bindParam(1, $this->Anio);
+    $sth->execute();
+    return $sth->fetchAll();
+    }
+
+   public function ConsultarFechaMes()
+   {
+    $sql= 'CALL RU_ReportePorMes(?)';
+    $sth = $this->db->prepare($sql);
+    $sth->bindParam(2, $this->Mes);
     $sth->execute();
     return $sth->fetchAll();
     }
